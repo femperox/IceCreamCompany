c3adf299dfbe56b29f510c0bdb805491af3c1a7c
femperox <ifilchukova1@gmail.com>
2021-09-01 17:51:51 +0300

    Представления для статистики. Вспомогательные функции

---
+++ sqlFiles/Misc.sql
@@ -0,0 +1,10 @@
+/*
+Возвращает из Date номер месяца
+Date - дата
+*/
+create or replace function getMonth(Date Date)
+returns integer as $$
+   select extract(month from date);
+$$ LANGUAGE sql
+
+
--- sqlFiles/Views.sql
+++ sqlFiles/Views.sql
@@ -3,7 +3,7 @@ Create or replace view AllOrders as
 	select Customer.Name as Company_Name, Orders.Date, Orders.Status, Orders.Price 
     from Orders
     Join customer on Orders.idcustomer = customer.id
-	order by date desc, status, price
+	order by date desc, status, price;
 
 -- Все продукты
 create or replace view AllProducts as
@@ -26,7 +26,7 @@ create or replace view AllIngsInProduct as
 	join ProductHasIngr on Ingredient.id =ProductHasIngr.idIngredient
 	join Product on Product.id =ProductHasIngr.idProduct
 
-	order by ProductHasIngr.idProduct
+	order by ProductHasIngr.idProduct;
 	
 -- Все продукты в заказе	
 create or replace view AllProductsInOrder as
@@ -40,10 +40,20 @@ create or replace view AllProductsInOrder as
 	from Product
 	join OrdersHaveProduct on OrdersHaveProduct.idProduct = Product.Id
 	join Orders on Orders.Id = OrdersHaveProduct.idOrder
-	join Customer on Customer.id = Orders.idCustomer
+	join Customer on Customer.id = Orders.idCustomer;
 
+-- Для каждого магазина узнать среднюю сумму заказов в каждом месяце.
+create or replace view ShopAvrgOrderMonthly as
 
-
-
+	select distinct
+	  Customer.Name as Customer,
+	  getMonth(Orders.Date) as Month,
+	  AVG(Orders.Price) over(
+		  					   partition by Customer.Name, getMonth(Orders.Date)
+		  					   order by getMonth(Orders.Date)  
+	  						)
+	from Orders
+	join Customer on Orders.idCustomer = Customer.id
+	order by Customer, month
 
 
