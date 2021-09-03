e699f4ad2c476951360d6bec9806c9aa249358fc
femperox <ifilchukova1@gmail.com>
2021-09-03 20:54:34 +0300

    Доработка интерфейса. Взаимосвязь с Бд

--- gui/main.py
+++ gui/main.py
@@ -11,23 +11,38 @@ import sys
 class mainWindow(QtWidgets.QMainWindow):
 
     actions = {}
-    cursor = None
 
-    pasw = ""
-
-    def __init__(self, parent):
+    def __init__(self, parent, conn, pasw):
         super(mainWindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
         self.parent = parent
-        self.conn = None
-        self.userRole = None
-
+        self.conn = conn
+        self.pasw = pasw
+        self.cursor = conn.cursor()
         self.createActions()
 
         self.ui.menubar.addAction(self.actions['exitAction'])
         self.ui.btnTest.clicked.connect(self.showOrders)
+
+        self.ui.btnTop5.setVisible(False)
+        self.ui.btnTop5.clicked.connect(self.showTop5)
+        self.ui.btnBack.setVisible(False)
+        self.ui.btnBack.clicked.connect(self.showOrders)
+
+        query = 'select session_user;'
+
+        result = misc.selectFromBD(self.cursor, query)
+        self.userRole = result[0][0]
+
+        if self.userRole == 'genmanager':
+            dicts = self.ui.menubar.addMenu('Справочники')
+            dicts.addAction(self.actions['ingrs'])
+            dicts.addAction(self.actions['products'])
+            dicts.addAction(self.actions['customers'])
+
+        self.ui.menubar.addAction(self.actions['orders'])
 
         
     def createActions(self):
@@ -38,13 +53,14 @@ class mainWindow(QtWidgets.QMainWindow):
         self.actions['orders'] = QtWidgets.QAction('&Заказы', self)
         self.actions['orders'].triggered.connect(self.showOrders)
 
+        self.actions['ingrs'] = QtWidgets.QAction('&Ингридиенты', self)
+        self.actions['ingrs'].triggered.connect(self.showIngrs)
 
-    def showOrders(self):
+        self.actions['products'] = QtWidgets.QAction('&Продукты', self)
+        self.actions['products'].triggered.connect(self.showProducts)
 
-        query = 'select * from allOrders order by date;'
-        result = misc.selectFromBDWithConn(self.userRole, self.pasw, query)
-        labels = ['№ заказа', 'Компания', 'Дата', 'Статус', 'Цена']
-        self.fillTable(labels, result)
+        self.actions['customers'] = QtWidgets.QAction('&Заказчики', self)
+        self.actions['customers'].triggered.connect(self.showCustomers)
 
 
     def checkRole(self):
@@ -62,50 +78,85 @@ class mainWindow(QtWidgets.QMainWindow):
         self.parent.show()
 
 
-    def update(self, conn, pasw):
-        '''
-        Обновляет параметры главного окна в зависимости от ролей
-        :param conn:
-        :return:
-        '''
+    def fillTable(self, tableWidg, labels, items):
+       tableWidg.setRowCount(0)
 
-        self.conn = conn
-        self.cursor = conn.cursor()
-        self.pasw = pasw
-        query = 'select session_user;'
+       tableWidg.setEditTriggers(QtWidgets.QTableWidget.NoEditTriggers)
+       tableWidg.setColumnCount(len(labels))
+       tableWidg.setHorizontalHeaderLabels(labels)
 
-        result = misc.selectFromBD(self.cursor, query)
-        self.userRole = result[0][0]
-
-        if self.userRole == 'genmanager':
-            pass
-
-        self.ui.menubar.addAction(self.actions['orders'])
-
-        
-
-    def fillTable(self, labels, items):
-
-       self.ui.tableWidget.setColumnCount(len(labels))
-       self.ui.tableWidget.setHorizontalHeaderLabels(labels)
-
+       tableWidg.cellDoubleClicked.connect(self.showNewTable)
+       if labels[0] != '№ заказа':
+           tableWidg.cellDoubleClicked.disconnect()
 
        for item in items:
-           row = self.ui.tableWidget.rowCount()
-           self.ui.tableWidget.setRowCount(row+1)
+           row = tableWidg.rowCount()
+           tableWidg.setRowCount(row+1)
 
-           self.ui.tableWidget.setItem(row, 0, QtWidgets.QTableWidgetItem(str(item[0])))
-           self.ui.tableWidget.setItem(row, 1, QtWidgets.QTableWidgetItem(item[1]))
-           self.ui.tableWidget.setItem(row, 2, QtWidgets. QTableWidgetItem(str(item[2])))
-           self.ui.tableWidget.setItem(row, 3, QtWidgets. QTableWidgetItem(item[3]))
-           self.ui.tableWidget.setItem(row, 4, QtWidgets. QTableWidgetItem(item[4]))
+           for i in range(len(item)):
+             tableWidg.setItem(row, i, QtWidgets.QTableWidgetItem(str(item[i])))
+
+
+    def showNewTable(self, row, column):
+
+        id = self.ui.tableWidget.item(row,0).text()
+
+        #self.ui.tableWidgetMore.setVisible(True)
+        self.ui.btnBack.setVisible(True)
+
+        query = 'select product, productAmount, price, totalPrice from allProductsInOrder where orderId = {0}'.format(id)
+        result = misc.selectFromBDWithConn(self.userRole, self.pasw, query)
+        labels = ['Продукт', 'Кол-во', 'Цена', 'Итоговая цена']
+
+        self.fillTable(self.ui.tableWidget, labels, result)
+
+
+    def showIngrs(self):
+        self.ui.btnTop5.setVisible(True)
+        query = 'select * from allIngredients;'
+        result = misc.selectFromBDWithConn(self.userRole, self.pasw, query)
+        labels = ['Название', 'Цена', 'Информация']
+        self.fillTable(self.ui.tableWidget,labels, result)
+
+    def showTop5(self):
+        self.ui.btnBack.setVisible(True)
+        self.ui.btnTop5.setVisible(False)
+        self.ui.btnBack.clicked.connect(self.showIngrs)
+
+        query = 'select * from Top5IngsMonthly'
+        result = misc.selectFromBDWithConn(self.userRole, self.pasw, query)
+        labels = ['Месяц', 'Продукты']
+        self.fillTable(self.ui.tableWidget, labels, result)
+
+    def showOrders(self):
+        self.ui.btnBack.setVisible(False)
+
+        query = 'select * from allOrders order by date;'
+        result = misc.selectFromBDWithConn(self.userRole, self.pasw, query)
+        labels = ['№ заказа', 'Компания', 'Дата', 'Статус', 'Цена']
+        self.fillTable(self.ui.tableWidget,labels, result)
+
+    def showProducts(self):
+
+        query = 'select * from allProducts;'
+        result = misc.selectFromBDWithConn(self.userRole, self.pasw, query)
+        labels = ['Название', 'Цена', 'Информация']
+        self.fillTable(self.ui.tableWidget,labels, result)
+
+    def showCustomers(self):
+
+        query = 'select * from allCustomers;'
+        result = misc.selectFromBDWithConn(self.userRole, self.pasw, query)
+        labels = ['Компания', 'Телефон']
+        self.fillTable(self.ui.tableWidget,labels, result)
+
 
 class loginDialog(QtWidgets.QDialog):
 
     def __init__(self):
         super(loginDialog, self).__init__()
-        self.main = mainWindow(self)
         self.ui = Ui_Dialog()
+        self.main = None
         self.ui.setupUi(self)
 
         self.setWindowTitle('Вход в систему')
@@ -120,8 +171,9 @@ class loginDialog(QtWidgets.QDialog):
         userPassw = self.ui.linePassword.text()
 
         try:
+
             self.conn = misc.connect(userName, userPassw)
-            self.main.update(self.conn, userPassw)
+            self.main = mainWindow(self, self.conn, userPassw)
 
             self.destroy()
             self.close()
